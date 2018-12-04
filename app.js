925fcdbfe0c39e5a98b0b27b4d5cd452e5ae944f
Ivelise Sola <solaivelise@yahoo.com>
2018-12-04 11:31:53 -0500

    traverse tree depth

--- app.js
+++ app.js
@@ -9,3 +9,14 @@ function Tree(data){
     var node = new Node(data);
     this._root = node;
 }
+
+Tree.prototype.traverseVTree = function(callback){
+
+    (function traverseV(currentNode){
+        for (var i = 0; i < currentNode.children.length; i++ ){
+            traverseV(currentNode.children[i]);
+        }
+        callback(currentNode);
+    })
+    (this._root);
+}
