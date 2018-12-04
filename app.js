475745f5c0e78f0ec977ce61cd0f37cefd5095ff
Ivelise Sola <solaivelise@yahoo.com>
2018-12-04 15:55:08 -0500

    tree methods

--- app.js
+++ app.js
@@ -1,4 +1,99 @@
 
+
+// class Node {
+//     constructor(data){
+//         this.data = data;
+//         this.parent = null;
+//         this.children = [];
+//     }
+// }
+
+// class Tree {
+//     constructor(node){
+//         this.root = node
+//     }
+
+
+// traverseDTree (callback) {
+//     /*
+//     I could use a while loop to check for depth < 3, 
+//     and call the traverseD function in it,
+//     my tree has a known detph = 2, but for this, 
+//     the function always need to be call from the root.
+//     */
+//     (function traverseD(currentNode) {
+//         for (let i = 0; i < currentNode.children.length; i++) {
+//             traverseD(currentNode.children[i]);
+//         }
+//         callback(currentNode);
+//     })
+//     (this.root);
+// }
+
+// containValue (callback, traversal) { traversal.call(this, callback) };
+
+
+// add (data, parentData, traversal) {
+//     let child = new Node(data);
+//     let parent = null;
+        
+//     let callback = (node) => {
+//             if (node.data === parentData) {
+//                 parent = node;
+//             }
+//         };
+//     this.containValue(callback, traversal);
+
+//     if (parent) {
+//         parent.children.push(child);
+//         child.parent = parent;
+//     } else {
+//         throw new Error('Cannot add node to a non-existing parent.')
+//     }
+// };
+
+// remove(data, parentData, traversal){
+//     let tree = this,
+//         parent = null,
+//         childToRemove = null,
+//         index;
+
+//     let callback = (node) => {
+//         if (node.data === parentData){
+//             parent = node;
+//         }
+//     };
+//     this.containValue(callback, traversal);
+
+//     if (parent){
+//         index = parent.children.findIndex(parent.children.data === data);
+
+//         if(index === undefined){
+//             throw new Error('Node to remove does not exist.');
+//         } else {
+//             childToRemove = paent.children.splice(index, 1);
+//         }
+//     } else {
+//         throw new error('Parent does not exist.');
+//     }
+//     return childToRemove;    
+// }
+
+// }
+
+//  let tree = new Tree('CEO')
+// // //let tree = new Tree(node);
+
+// tree.add('VP of Happiness', 'CEO', tree.traverseDTree);
+// tree.add('VP of Finance', 'CEO', tree.traverseDTree);
+// tree.add('VP of Sadness', 'CEO', tree.traverseDTree);
+ 
+// tree.add('Director of Puppies', 'VP of Finance', tree.traverseDTree);
+// tree.add('Manager of Puppies', 'Director of Puppies', tree.traverseDTree);
+// console.log(tree);
+
+
+
 function Node(data) {
     this.data = data;
     this.parent = null;
@@ -74,3 +169,25 @@ Tree.prototype.remove = function(data, parentData, traversal){
     }
     return childToRemove;    
 }
+
+findIndex = (arr, data) => {
+    var index;
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i].data === data){
+            index = i;
+        }
+    }
+    return index;
+
+}
+
+let tree1 = new Tree('CEO')
+
+tree1.add('VP of Happiness', 'CEO', tree1.traverseDTree);
+tree1.add('VP of Finance', 'CEO', tree1.traverseDTree);
+tree1.add('VP of Sadness', 'CEO', tree1.traverseDTree);
+ 
+tree1.add('Director of Puppies', 'VP of Finance', tree1.traverseDTree);
+tree1.add('Manager of Puppies', 'Director of Puppies', tree1.traverseDTree);
+
+console.log(tree1.root.children[1].children[0].data);
